bea3797b2ac7ddb3de7402e8b2b2ba88d53b6472
xprabhudayal <128473068+xprabhudayal@users.noreply.github.com>
2024-07-13 10:12:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from setuptools import setup
 setup(
     name='xs',
     version='0.0.1',
-    description='a module for creation of endpoint of the open-sourced llms like meta llama3, mistral, neogpt, and more...',
-    url='git@github.com:xprabhudayal/xs.git',
+    description='A module for creation of endpoint of the open-sourced LLMs like Meta LLaMA3, Mistral, NeoGPT, and more...',
+    url='https://github.com/xprabhudayal/xs.git',
     author='Prabhudayal Vaishnav',
     author_email='pradachan@tuta.io',
     license='MIT',
-    packages=['xs'],
+    py_modules=['xs'],
+    install_requires=[
+        'flask',
+        'pyngrok',
+        'transformers'
+    ],
     zip_safe=False
 )
