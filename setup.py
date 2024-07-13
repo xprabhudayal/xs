61fba0c0e25f6d595fe5c7133bedea5ed9e46644
xprabhudayal <128473068+xprabhudayal@users.noreply.github.com>
2024-07-13 10:02:19 +0530

    main module config commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xs',
+    version='0.0.1',
+    description='a module for creation of endpoint of the open-sourced llms like meta llama3, mistral, neogpt, and more...',
+    url='git@github.com:xprabhudayal/xs.git',
+    author='Prabhudayal Vaishnav',
+    author_email='pradachan@tuta.io',
+    license='MIT',
+    packages=['xs'],
+    zip_safe=False
+)
