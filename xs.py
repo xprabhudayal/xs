be23aab6f3decd6387334a7b89b1f587cfcf1f12
xprabhudayal <128473068+xprabhudayal@users.noreply.github.com>
2024-07-28 15:21:39 +0530

    Update xs.py

--- xs.py
+++ xs.py
@@ -65,6 +65,9 @@ def connect_text(public_url, data):
         print("Failed to decode JSON response")
         print(response.text) 
 
+#empty generate checking 
+def generate(prompt):
+  pass
 
 #this is a specific route for the api(endpoint) in which we can post prompt and get inference responses
 #YOUR LLM GENERATION FUNCTION SHOULD BE NAMED WITH :: generate :: elsewise it wont work
